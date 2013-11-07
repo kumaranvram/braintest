18bfe8891d63b169288e73b6095199f4addf7bb9
nehashri <nehashrl@rohithr.corporate.thoughtworks.com>
2013-11-07 15:11:05 +0530

    Added more questions

--- js/braintest.js
+++ js/braintest.js
@@ -211,6 +211,279 @@ var loadJSON = function() {
 				"answer": "true"
 			},
 			"timeout": "5",
-		}
+		},
+		{
+		"question": {
+			"text": "Before Mt. Everest was discovered, what was the highest mountain in the world?"
+		},
+		"answerLeft": {
+			"text": "Mt. K2"
+		},
+		"answerRight": {
+			"text": "Mt. Everest",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "Which is correct to say, 'The yolk of the egg is white' or 'The yolk of the egg are white?'"
+		},
+		"answerLeft": {
+			"text": "Both"
+		},
+		"answerRight": {
+			"text": "Neither",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "A farmer has five haystacks in one field and four haystacks in another. How many haystacks would he have if he combined them all in one field?"
+		},
+		"answerLeft": {
+			"text": "One",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "Nine"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "A bottle of juice cost  Rs 10. The juice was worth Rs 9 more than the bottle.How much was the bottle worth?"
+		},
+		"answerLeft": {
+			"text": "1 rupee"
+		},
+		"answerRight": {
+			"text": "0.50 rupees",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "There is a house with four walls. Each wall faces south. There is a window in each wall.A bear walks by one of the windows. What colour is the bear?"
+		},
+		"answerLeft": {
+			"text": "Brown"
+		},
+		"answerRight": {
+			"text": "White",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "Some months have 31 days; how many have 28?"
+		},
+		"answerLeft": {
+			"text": "One"
+		},
+		"answerRight": {
+			"text": "All",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "In the 1750's, was it legal for a man in India to marry his widow's sister?"
+		},
+		"answerLeft": {
+			"text": "Yes"
+		},
+		"answerRight": {
+			"text": "No",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "Divide 30 by 1/2 and add 10. What is the answer?"
+		},
+		"answerLeft": {
+			"text": "70",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "25"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "An electric train is going 40 mph north, the wind is blowing 60 mph south. Which way is the train's smoke blowing?"
+		},
+		"answerLeft": {
+			"text": "I have no idea",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "North"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "A farmer has 17 sheep, and all but 9 die. How many are left?"
+		},
+		"answerLeft": {
+			"text": "8"
+		},
+		"answerRight": {
+			"text": "9",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "Now that the Soviet Union has broken up, what is the world's largest country by land area?"
+		},
+		"answerLeft": {
+			"text": "Russia",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "China"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "In what year did Christmas, and News Year's fall on the same day?"
+		},
+		"answerLeft": {
+			"text": "Always",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "None"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "What 9 letter English word is still a word each time you take away a letter?"
+		},
+		"answerLeft": {
+			"text": "Startling",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "Starting"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "How many times can you subtract the number 5 from 25?"
+		},
+		"answerLeft": {
+			"text": "Five"
+		},
+		"answerRight": {
+			"text": "One",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "At the end of a banquet 10 people shake hands with each other. How many handshakes will there be in total?"
+		},
+		"answerLeft": {
+			"text": "50",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "100"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "The day before the day before yesterday is three days after Saturday. What day is it today?"
+		},
+		"answerLeft": {
+			"text": "Saturday"
+		},
+		"answerRight": {
+			"text": "Friday",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "The word 'racecar' is spelled the same forwards and backwards."
+		},
+		"answerLeft": {
+			"text": "True",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "False"	
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "Mary's father had 5 children: <h2>Mimi, Mumu, Mama, Meme.</h2> What was the 5th child's name?"
+		},
+		"answerLeft": {
+			"text": "Mini"
+		},
+		"answerRight": {
+			"text": "Mary",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "A murderer is condemned to death. He has to choose between two rooms. The first is full of raging fires, second is full of lions that haven't eaten in 3 years. Which room is safest for him?"
+		},
+		"answerLeft": {
+			"text": "First room"
+		},
+		"answerRight": {
+			"text": "Second room",
+			"answer": "true"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "Are 1990 Indian rupees worth more than 1989 Indian rupees?"
+		},
+		"answerLeft": {
+			"text": "True",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "False"
+		},
+		"timeout": "5",
+	},
+	{
+		"question": {
+			"text": "If it take 5 minutes to dry a t-shirt on a clothes wire, how long would it take to dry 5 t-shirts?"
+		},
+		"answerLeft": {
+			"text": "5 minutes",
+			"answer": "true"
+		},
+		"answerRight": {
+			"text": "25 minutes"
+		},
+		"timeout": "5",
+	}
 	];
 }
